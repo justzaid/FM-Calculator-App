e37340f18eb8c7c943d9716759b8c7ae11763d5d
justzaidlaptop <zaidmk96@gmail.com>
2026-03-12 07:20:32 +0300

    CSS fun, testing theme toggler with DOM

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let themeOne = document.getElementById('themeOneDial')
+let themeTwo = document.getElementById('themeTwoDial')
+let themeThree = document.getElementById('themeThreeDial')
+const del = document.getElementById('del')
+
+
+function switchDials() {
+    if (themeOne.checked && !themeTwo.checked && !themeThree.checked) {
+        themeOne.checked = false
+        themeTwo.checked = true
+        return
+    }
+    if (themeTwo.checked && !themeOne.checked && !themeThree.checked) {
+        themeTwo.checked = false
+        themeThree.checked = true
+        return
+    }
+    if (themeThree.checked && !themeOne.checked && !themeTwo.checked) {
+        themeThree.checked = false
+        themeOne.checked = true
+        return
+    }
+    console.log(themeOne.checked)
+}
+
+
+del.addEventListener('click', switchDials)
+
+
+
+
